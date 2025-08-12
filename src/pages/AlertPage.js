2fb4f655b2b07239c525553dfe998c49e880dbdf
mjd0530 <mjd0530@gmail.com>
2025-08-12 16:06:31 -0400

    Fix toast alert component example to show timestamp for advanced variants

--- src/pages/AlertPage.js
+++ src/pages/AlertPage.js
@@ -287,7 +287,7 @@ const AlertPage = () => {
 
         <PreviewContainer theme={theme}>
           <Alert
-            type={ALERT_TYPES.INLINE}
+            type={ALERT_TYPES.TOAST}
             variant={variant}
             severity={severity}
             theme={theme}
