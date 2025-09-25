8e9cc3708b6c35d7adede7f4379ae5ce54d9ee90
mjd0530 <mjd0530@gmail.com>
2025-09-25 08:52:54 -0400

    Typography: simplify font stack to 'Noto Sans', system-ui, sans-serif

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const fontStack = '"Noto Sans", system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans CJK", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif';
+export const fontStack = '"Noto Sans", system-ui, sans-serif';
 
 // Define the background.canvas color token
 export const tokens = {
